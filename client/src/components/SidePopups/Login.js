8433585b460743fca499e7177fcc76caa39c52be
Marius Schmidt-Mengin <marius.schmidt-mengin@eleves.enpc.fr>
2018-12-03 17:28:37 +0100

    Désinstallation de material-ui Installation de @material-ui/core Modifications dans Login.js Ajout de Login.css

--- client/src/components/SidePopups/Login.js
+++ client/src/components/SidePopups/Login.js
@@ -1,32 +1,33 @@
 import React, {Component} from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+//import RaisedButton from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import styles from './Login.css'
 
 class Login extends Component{
   constructor(props) {
 		super(props)
 		this.state = {
-			username: '',
-      password: ''
+			username: 'test_user',
+      		password: 'test_pswd'
 		}
 	}
 
   render() {
     return (
-      <div>
+      <div className={styles.Login}>
         <MuiThemeProvider>
           <div>
            <TextField
-             hintText="Enter your Username"
-             floatingLabelText="Username"
+             placeholder="Enter your Username"
+             helperText="Username"
              onChange = {(event,newValue) => this.setState({username:newValue})}
              />
            <br/>
              <TextField
                type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
+               placeholder="Enter your Password"
+               helperText="Password"
                onChange = {(event,newValue) => this.setState({password:newValue})}
                />
           <br/>
